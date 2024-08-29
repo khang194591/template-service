7b15c5f9a3a3ce801415563cb355752715f19b47
khang <khang194591@gmail.com>
2024-08-29 13:29:13 +0700

    update

---
+++ scripts/templates/__t__.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class __T__Repository {
+  constructor() {}
+
+  async create() {}
+
+  async find() {}
+
+  async findById() {}
+
+  async update() {}
+
+  async delete() {}
+}
---
+++ scripts/templates/__t__.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from "@nestjs/common";
+import { __T__Repository } from "./__t__.repository";
+
+@Injectable()
+export class __T__Service {
+  constructor(private readonly __t__Repository: __T__Repository) {}
+}
